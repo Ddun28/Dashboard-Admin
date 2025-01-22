ca6507285d6bcd241535544fd96ef71705fc89f6
Daniel Dun <danieldum28@gmail.com>
2025-01-21 21:05:07 -0400

    fix

--- app/(routes)/tasks/components/Calendar/Calendar.tsx
+++ app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -89,7 +89,7 @@ export default function Calendar(props : CalendarProps) {
             });
             setOnSaveNewEvent(false);
         }
-    }, [onSaveNewEvent, selectedItem, events]);
+    }, [onSaveNewEvent, selectedItem, event]);
 
   return (
     <div>
